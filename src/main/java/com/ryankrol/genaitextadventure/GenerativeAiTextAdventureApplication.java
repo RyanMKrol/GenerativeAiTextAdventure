31d17dd58b6448271b372db8c703cfc51614fb6d
RyanMKrol <ryankrol.m@gmail.com>
2024-02-04 22:11:15 +0000

    Init

---
+++ src/main/java/com/ryankrol/genaitextadventure/GenerativeAiTextAdventureApplication.java
@@ -0,0 +1,13 @@
+package com.ryankrol.genaitextadventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerativeAiTextAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerativeAiTextAdventureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryankrol/genaitextadventure/GenerativeAiTextAdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryankrol.genaitextadventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerativeAiTextAdventureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
